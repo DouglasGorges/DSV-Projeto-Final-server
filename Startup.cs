fde41bf603d1f738523745cc525849e7ab63912d
DouglasGorges <douglasgorges@icloud.com>
2021-09-02 20:00:36 -0300

    Alteracoes referentes a aula do dia 26-08-21

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
 
 namespace backend
 {
@@ -26,6 +28,11 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<DataContext>
+            (
+                options => options.UseInMemoryDatabase("databaseString")
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
