d2ad53b32195356d7fdd123d6783b3faed82cd77
DouglasGorges <douglasgorges@icloud.com>
2021-09-03 09:28:55 -0300

    REmovidos comentarios desnecessarios

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -16,7 +16,6 @@ namespace server.Controllers
         //Construtor
         public ProdutoController(DataContext context) => _context = context;
 
-        //POST: api/produto/create
         [HttpPost]
         [Route("create")]
         public Produto Create(Produto produto)
@@ -26,9 +25,36 @@ namespace server.Controllers
             return produto;
         }
 
-        //GET: api/produto/list
         [HttpGet]
         [Route("list")]
         public List<Produto> List() => _context.Produtos.ToList();
+
+        [HttpGet]
+        [Route("findById/{id?}")]
+        public Produto GetById(int id) => _context.Produtos.Find(id);
+
+        [HttpPost]
+        [Route("edit")]
+        public Produto Edit(Produto produto)
+        {
+            Produto produtoOriginal = GetById(produto.Id);
+
+            produtoOriginal.Descricao = produto.Descricao;
+            //E outros atributos
+
+            _context.Produtos.Update(produtoOriginal);
+            _context.SaveChanges();
+            return produtoOriginal;
+        }
+
+        [HttpPost]
+        [Route("remove/{id?}")]
+        public Produto Remove(int id)
+        {
+            Produto produto = GetById(id);
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return produto;
+        }
     }
 }
